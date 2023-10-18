821c8c7d7a6d526e2b93149860c92602d03cec20
alaurai <alauraivani@gmail.com>
2023-10-18 15:43:54 -0300

    feat: config Firebase

--- android/app/src/main/java/com/al/rockethelp/MainActivity.java
+++ android/app/src/main/java/com/al/rockethelp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rockethelp;
+package com.al.rockethelp;
 
 import android.os.Build;
 import android.os.Bundle;
--- android/app/src/main/java/com/al/rockethelp/MainApplication.java
+++ android/app/src/main/java/com/al/rockethelp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rockethelp;
+package com.al.rockethelp;
 
 import android.app.Application;
 import android.content.res.Configuration;
